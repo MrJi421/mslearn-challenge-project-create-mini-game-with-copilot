7d8ddfc08b77c5cc78e4f516b648eb928ee01236
MrJi421 <mr.ji421@outlook.com>
2023-11-11 17:15:25 +0530

    Commit from mrji421

--- app.py
+++ app.py
@@ -8,40 +8,18 @@
 
 import random
 
-# Define the options
+# Define the game options
 options = ["rock", "paper", "scissors"]
 
-# Define the function to determine the winner
-def determine_winner(option1, option2):
-    if option1 == option2:
-        return "tie"
-    elif option1 == "rock" and option2 == "scissors":
-        return "player 1 wins"
-    elif option1 == "paper" and option2 == "rock":
-        return "player 1 wins"
-    elif option1 == "scissors" and option2 == "paper":
-        return "player 1 wins"
-    elif option1 == "rock" and option2 == "paper":
-        return "player 2 wins"
-    elif option1 == "paper" and option2 == "scissors":
-        return "player 2 wins"
-    else:
-        return "player 1 wins"
+# Define the function to play the game
+def play_game():
+    # Initialize the score
+    score = {"wins": 0, "rounds": 0}
 
-# Define the main function
-def main():
-    # Print the game instructions
-    print("Welcome to the rock-paper-scissors game!")
-    print("Here are the rules:")
-    print("Rock beats scissors.")
-    print("Scissors beat paper.")
-    print("Paper beats rock.")
-    print("--------------------------------------------------")
-
-    # Loop until the game is over
+    # Loop until the minigame is terminated
     while True:
         # Get the player's choice
-        player_choice = input("Player 1, please choose rock, paper, or scissors: ").lower()
+        player_choice = input("Choose rock, paper, or scissors: ").lower()
 
         # Check if the player's choice is valid
         if player_choice not in options:
@@ -51,28 +29,34 @@ def main():
         # Get the computer's choice
         computer_choice = random.choice(options)
 
-        # Determine the winner
-        winner = determine_winner(player_choice, computer_choice)
-
-        # Print the results
-        print("Player 1 chose:", player_choice)
+        # Print the player's and computer's choices
+        print("You chose:", player_choice)
         print("Computer chose:", computer_choice)
-        print("--------------------------------------------------")
 
-        # Check if the game is over
-        if winner == "tie":
+        # Determine the winner
+        if player_choice == computer_choice:
             print("It's a tie!")
-        elif winner == "player 1 wins":
-            print("Player 1 wins!")
+        elif player_choice == "rock" and computer_choice == "scissors":
+            print("You win!")
+            score["wins"] += 1
+        elif player_choice == "paper" and computer_choice == "rock":
+            print("You win!")
+            score["wins"] += 1
+        elif player_choice == "scissors" and computer_choice == "paper":
+            print("You win!")
+            score["wins"] += 1
         else:
             print("Computer wins!")
+
+        # Increment the round count
+        score["rounds"] += 1
+
+        # Ask the player if they want to continue
+        if input("Do you want to continue? (y/n): ").lower()!= "y":
             break
 
-        # Ask the player if they want to play again
-        play_again = input("Do you want to play again? (y/n): ").lower()
-        if play_again!= "y":
-            break
+    # Print the final score
+    print("Final score:", score["wins"], "wins", score["rounds"], "rounds")
 
-# Call the main function
-if __name__ == "__main__":
-    main()
+# Call the play_game function to start the game
+play_game()
